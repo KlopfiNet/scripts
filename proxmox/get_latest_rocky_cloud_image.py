2a9d6fc2acc94bbe12a3870ae3da583be0b6e899
The Man <ThisIsntTheWay@users.noreply.github.com>
2023-03-05 15:41:33 +0100

    Increase verbosity

--- proxmox/get_latest_rocky_cloud_image.py
+++ proxmox/get_latest_rocky_cloud_image.py
@@ -12,7 +12,7 @@ import sys
 ###################################
 imageUrl   = "http://download.rockylinux.org/pub/rocky/9.1/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2"
 imageCheck = "http://download.rockylinux.org/pub/rocky/9.1/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2.CHECKSUM"
-targetFile = "/mnt/pve/source/Rocky-9-GenericCloud.latest.x86_64.qcow2"
+targetFile = "/mnt/pve/source/images/Rocky-9-GenericCloud.latest.x86_64.qcow2"
 
 def get_remote_image():
     print("Getting remote image...")
@@ -23,7 +23,7 @@ def get_remote_image():
             urllib.request.urlretrieve(imageCheck, (targetFile + ".CHECKSUM"))
             print("> Got file.")
         except Exception as err:
-            sys.exit("Error downloading file: " + err)
+            sys.exit("Error downloading file: " + str(err))
     else:
         print("> File already exists.")
 
@@ -33,7 +33,7 @@ def get_remote_checksum():
         urllib.request.urlretrieve(imageCheck, (targetFile + ".CHECKSUM"))
         print("> Got checksum.")
     except Exception as err:
-        sys.exit("Error downloading checksum: " + err)
+        sys.exit("Error downloading checksum: " + str(err))
         
 def get_sha256_checksum(file):
     print("Calculating file hash...")
@@ -62,14 +62,18 @@ with open((targetFile + ".CHECKSUM"), 'r') as f:
 
 actual_hash = get_sha256_checksum(targetFile)
 
+print("Beginning checksum validation...")
 if actual_hash == wanted_hash:
-    print(targetFile + " checksum validation OK.")
+    print(f"{targetFile} checksum validation OK")
 else:
-    print(targetFile + " checksum validation FAIL")
-    print("> Wanted : " + wanted_hash)
-    print("> Got    : " + actual_hash)
+    print(f"{targetFile} checksum validation FAIL")
+    print(f"> Wanted : {wanted_hash}")
+    print(f"> Got    : {actual_hash}")
     
+    print("Removing and reaquiring image...")
     os.remove(targetFile)
     get_remote_image()
     get_remote_checksum()
-    
+
+print("")
+print("Script is done.")
